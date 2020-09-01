0505a736a11ce3279cf1e06ae2828424401f062f
Aafreen Sayyed <Aafreen.sayyed@aexp.com>
2020-09-01 23:10:58 +0530

    NavBar(underline issue, scroll) , Autocomplete, Accordian, React Table, SnackBar functionality Fixes by Aafreen Sayyed

--- ReactAtom/src/components/Autocomplete/Search.js
+++ ReactAtom/src/components/Autocomplete/Search.js
@@ -12,29 +12,13 @@ class Search extends Component {
           <h4>The autocomplete is a normal text input enhanced by a panel of suggested options.</h4>
           <hr className="hr"/>
 
-          <div>
-              <h2>Combo box</h2>
+              <h2>Country Search</h2>
               <hr className="hr"/>
-              <h4>The value must be chosen from a predefined set of allowed values.</h4>
+              <h4>This is a basic example of search auto-complete which uses onChange event and fetches result</h4>
               <hr className="hr"/>
               <ComboBox/>
-          </div>
 
-          <div>
-              <h2>Country select</h2>
-              <hr className="hr"/>
-              <h4>Choose one of the 248 countries.</h4>
-              <hr className="hr"/>
-              <CountrySelect/>
-          </div>
 
-          <div>
-              <h2>Creatable</h2>
-              <hr className="hr"/>
-              <h4>A option, for instance Add "YOUR SEARCH". You could also display a dialog when the user wants to add a new value..</h4>
-              <hr className="hr"/>
-              <FreeSoloCreateOptionDialog/>
-          </div>
 
       </div>
     );
