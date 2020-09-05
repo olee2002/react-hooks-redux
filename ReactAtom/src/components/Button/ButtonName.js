7674f2f42a1487b803be9044638fad116413919d
babylee2002 <babylee2002@gmail.com>
2020-09-05 13:29:07 -0400

    adding project example

--- ReactAtom/src/components/Button/ButtonName.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Button = (props) => {
- return (
-  <button className={props.className} onClick={props.onClick}> {props.buttonText} </button> 
-)}
-
-export default Button;
