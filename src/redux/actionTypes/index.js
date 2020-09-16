596a3eedc4721f29931d8222911b252ea5fe473b
VP5019447 <vibha_pandey@syntelinc.com>
2020-09-16 00:40:56 -0400

    Update index.js

--- src/redux/actionTypes/index.js
+++ src/redux/actionTypes/index.js
@@ -1,3 +1,5 @@
+//action type is a message we dispatch to the store.
+
 export const ADD_ITEM = "ADD_ITEM";
 export const DELETE_ITEM = "DELETE_ITEM";
 export const FETCH_TODO_LIST = "FETCH_TODO_LIST";
