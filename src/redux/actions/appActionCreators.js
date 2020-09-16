2984386fdc15c4af489b5b5e0da89e1d29fff59a
VP5019447 <vibha_pandey@syntelinc.com>
2020-09-16 00:45:22 -0400

    Update appActionCreators.js

--- src/redux/actions/appActionCreators.js
+++ src/redux/actions/appActionCreators.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 import { FETCH_TODO_LIST } from '../actionTypes';
 
+// creating an action creator for an async action and dispatch it to the store (Thunk middleware enables the dispatch) 
 export const fetchToDoList =()=> (dispatch)=>{
 axios.get('https://jsonplaceholder.typicode.com/todos')
 .then(res=> {
