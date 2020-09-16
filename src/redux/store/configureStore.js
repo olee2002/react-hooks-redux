cb8a138d5dbce887e535ad0e21513035a8c6789e
VP5019447 <vibha_pandey@syntelinc.com>
2020-09-16 00:38:40 -0400

    Update configureStore.js

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -4,9 +4,11 @@ import ReduxThunk from 'redux-thunk'
 
 import rootReducer from '../reducers'
 
+// pass middlewares in the array
 const middleware = [ReduxThunk];
 
 export default function configureStore() {
+   // createStore function requires rootReducers as the first arg and middlewares in arr as second but you can also add compose to pass Redux dev tool.
    return createStore(rootReducer, 
       compose(
           applyMiddleware(...middleware),
